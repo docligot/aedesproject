628a5577d0e607a04c894d174035c1af909d8f9c
Dominic Ligot <dominic.ligot@cirrolytix.com>
2019-10-20 11:20:29 +0800

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -17,6 +17,16 @@
       }
 
     </style>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-126660213-5"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-126660213-5');
+	</script>	
   </head>
     <header class="">
       <div class="">
